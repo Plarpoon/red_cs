1e1d0f4c8a1c467c53afe4c2363c578b326be268
Alessandro Suha <github@plarp.net>
2024-05-02 21:56:29 +0200

    docs: add comments

--- Program.cs
+++ Program.cs
@@ -23,16 +23,23 @@ namespace EvilBot
             // Load all commands using reflection
             var commandTypes = Assembly.GetExecutingAssembly()
                 .GetTypes()
+                // Filter the types to only those in the "EvilBot.src.commands" namespace and have a method named "Execute"
                 .Where(t => t.Namespace == "EvilBot.src.commands" && t.GetMethods().Any(m => m.Name == "Execute"));
 
             foreach (var commandType in commandTypes)
             {
+                // Get the "Execute" method from the command type
                 var executeMethod = commandType.GetMethod("Execute");
+                // Check if the "Execute" method exists
                 if (executeMethod != null)
                 {
+                    // Create an instance of the command type if the "Execute" method is not static
+                    // If the "Execute" method is static, instance remains null
                     object? instance = executeMethod.IsStatic ? null : Activator.CreateInstance(commandType);
                     if (instance != null || executeMethod.IsStatic)
                     {
+                        // Add a new event handler to the MessageCreated event of the discord client
+                        // The event handler invokes the "Execute" method of the command type
                         discord.MessageCreated += new AsyncEventHandler<DiscordClient, MessageCreateEventArgs>((client, e) => (Task?)executeMethod.Invoke(instance, [e]));
                     }
                 }

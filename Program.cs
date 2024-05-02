ef3d577d89700c1ef56ebeafb5f205a591c32a39
Alessandro Suha <github@plarp.net>
2024-05-02 21:30:31 +0200

    feat: add DSharpPlus support

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DSharpPlus;
+using DotNetEnv;
+
+namespace EvilBot
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //  Load env variables from the .env file in the root directory
+            Env.Load();
+
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = Env.GetString("DISCORD_TOKEN"),
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents
+            });
+
+            discord.MessageCreated += async (s, e) =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong!");
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
